7aa936f16a78e0027c671b1611e72cf68f5ddcbc
Florian Omnès <florian.omnes@upmc.fr>
2018-05-16 11:16:34 +0200

    Modified least-square test-case

--- run_case.sh
+++ run_case.sh
@@ -7,7 +7,7 @@
 
 #Get rid of the somewhat excessive debugging info from FreeFem++
 export FF_VERBOSITY=0;
-FLAGS="--saveevery 3"
+FLAGS="--saveevery 1"
 FF=FreeFem++-nw
 case "$1" in
     1)
@@ -20,7 +20,7 @@ case "$1" in
 	;;
     3)
 	echo "Launching test-case 3"
-	$FF main.edp --config 3 --resu resu/case3 --binit 0 --btarget 0 --cv 1 --tau 1e-2 --l0 0 --raffinit 5e-2 --gamma .01 --jjmax 30 --delta 0 $FLAGS
+	$FF main.edp --config 3 --resu resu/case3 --binit 0 --btarget 0 --cv 1 --tau 5e-3 --l0 0 --raffinit 5e-2 --gamma .01 --jjmax 100 --delta 0 $FLAGS
 	;;
     4)
 	echo "Launching test-case 4"
